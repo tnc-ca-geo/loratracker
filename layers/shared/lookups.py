359cc2e45a739da0157ed12d63ee120ab9937f57
Falk Schuetzenmeister <falk_email@yahoo.com>
2022-09-06 10:22:30 -0700

    prepare merge

---
+++ layers/shared/lookups.py
@@ -0,0 +1,74 @@
+"""
+Shared lookups
+"""
+
+DEVICE_LABELS = {
+    'adeunis-ftd-23754': 'Mark Goering tester',
+    'feather-ranger-f3c3': 'The NO device',
+    'miromico-007a4f': 'JLDP test device (lost)',
+    'miromico-007a2f': 'JLDP #4 flatbed',
+    'miromico-007a2e': 'JLDP #7 Silverado',
+    'miromico-007a2d': 'JLDP #10 flatbed',
+    'miromico-007a2c': 'JLDP blue UTV',
+    'miromico-007a2b': 'SCI Miromico 2 unplaced',
+    'miromico-007a2a': 'SCI Miromico 1 unplaced',
+    'miromico-007a29': 'JLDP #2 Colorado',
+    'miromico-007a26': 'JLDP #5 flatbed',
+    'miromico-007a30': 'JLDP green UTV',
+    'miromico-007a25': 'JLDP black utv',
+    'oyster-005d93': 'SCI Blue 4runner',
+    'jldp-oyster-c7a8': 'JLDP Green Honda OHV',
+    'jldp-oyster-c7a0': 'JLDP Blue Kawasaki OHV',
+    'tnc-adeunis-ftd-0235a1': 'JLDP Kelly signal tester'}
+
+GATEWAY_LABELS = {
+    'dragino-dlos8-28260': 'TIS Dragino Mark home',
+    'c0ee40ffff296af5': 'Falk Laird home',
+    'laird-rg191-296af5': 'Falk Laird home',
+    'laird-rg1xx-293ebd': 'SCI Laird Diablo',
+    'laird-rg1xx-294d3c': 'SCI Laird Main Ranch',
+    'laird-rg1xx-294f2e': 'Staten Laird Office',
+    'laird_rg191_outdoor_staten': 'Staten Laird Office',
+    'lorix-one-0a8a23': 'TIS LorixOne Spare',
+    'lorix-one-0b6395': 'SCI LorixOne Centinela',
+    'lorix-one-206f01': 'JLDP LorixOne Sutter Peak',
+    'lorix-one-207446': 'JLDP LorixOne Jamala HQ',
+    'lorix-one-20d3a6': 'JLDP LorixOne Relay Ridge',
+    'lorix-one-207d24': 'SCI LorixOne Valley Peak',
+    'lorix-one-20bb57': 'JLDP Bunker Hill',
+    'lorix-one-2ddd4e': 'JLDP LorixOne unplaced',
+    'lorix-one-2ef2c4': 'JLDP LorixOne Kelly home',
+    'lorix-one-2e1135': 'JLDP LorixOne unplaced',
+    'lorix-one-2eb3b1': 'SCI LorixOne Diablo 2',
+    'lorix-one-2e901c': 'JLDP LorixOne unplaced',
+    'lorix-one-2de697': 'Maui LorixOne Waikamoi',
+    'lorix-one-2e89bd': 'JLDP LorixOne Cojo Water tanks',
+    'eui-58a0cbfffe801686': 'TTN indoor starter'}
+
+# domain lookup
+DOMAINS = {
+    'adeunis-ftd-23754': 'wa',
+    'miromico-007a4f': 'jldp',
+    'miromico-007a2f': 'jldp',
+    'miromico-007a2e': 'jldp',
+    'miromico-007a2d': 'jldp',
+    'miromico-007a2c': 'jldp',
+    'miromico-007a2b': 'sci',
+    'miromico-007a2a': 'sci',
+    'miromico-007a29': 'jldp',
+    'miromico-007a26': 'jldp',
+    'miromico-007a30': 'jldp',
+    'miromico-007a25': 'jldp',
+    '9876b6115b69': 'staten',
+    'raspverry_pi_ranger': 'falk',
+    '3939353476387418': 'falk',
+    'feather_ranger': 'falk',
+    'feather_ranger_2': 'falk',
+    'feather-ranger-f3c3': 'falk',
+    'oyster-005d93': 'sci',
+    'jldp-oyster-c7a8': 'jldp',
+    'jldp-oyster-c7a0': 'jldp',
+    'tnc-mmico-cargo-007a4f': 'jldp',
+    'tnc-mmico-tracker2-007a4e': 'jldp',
+    'tnc-adeunis-ftd-0235a1': 'jldp'
+}
